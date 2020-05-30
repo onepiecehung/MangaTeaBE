a90d56210406e4fd8067aa1ffea1e8b75d069914
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2020-05-30 21:44:48 +0700

    add search manga

--- packages/manga/manga.controller.js
+++ packages/manga/manga.controller.js
@@ -3,9 +3,9 @@ import * as MangaService from "./manga.service";
 
 export async function find(req, res) {
     try {
-        let data = await MangaService.find(req.query)
-        return response.success(res, data, 200)
+        let data = await MangaService.find(req.query);
+        return response.success(res, data, 200);
     } catch (error) {
-        return response.error(res, req, error)
+        return response.error(res, req, error);
     }
 }
--- packages/manga/manga.service.js
+++ packages/manga/manga.service.js
@@ -5,8 +5,20 @@ export async function find(keyword) {
     try {
         const {
             fromCreatedAt,
-            toCreatedAt
+            toCreatedAt,
+            genre,
+            id,
+            authorName,
+            isAdult,
+            country,
+            tags,
+            format
         } = keyword
+        if (id) {
+            let data = await MangaRepository.findById(id);
+            return data;
+
+        }
         const sort = keyword.sort || { createdAt: -1 }
         const limit = parseInt(keyword.limit) || 20
         const skip = parseInt(keyword.skip) || 0
@@ -16,6 +28,31 @@ export async function find(keyword) {
         }
         if (toCreatedAt) {
             filters.push({ createdAt: { $lte: new Date(toCreatedAt) } });
+        }
+        if (genre) {
+            if (Array.isArray(genre)) {
+                filters.push({ genres: { $all: genre } });
+            } else {
+                filters.push({ genres: genre });
+            }
+        }
+        if (authorName) {
+            filters.push({ "staff.name": authorName });
+        }
+        if (isAdult) {
+            filters.push({ isAdult: isAdult });
+        }
+        if (country) {
+            filters.push({ countryOfOrigin: country });
+        }
+        if (tags) {
+            filters.push({ "tags.name": tags });
+        }
+        if (format) {
+            filters.push({ format: format });
+        }
+        if (status) {
+            filters.push({ status: status });
         }
         let [manga, total] = await Promise.all([
             MangaRepository.find(filters, limit, skip, sort),
