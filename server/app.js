0eb282f65d6a1f9e9da550e726985fcb5b691b0d
Dung Ha Tien <111dung1111@gmail.com>
2020-08-08 16:38:59 +0700

    fix cors allow methods

--- server/app.js
+++ server/app.js
@@ -43,6 +43,7 @@ app.use(Sentry.Handlers.errorHandler());
 // app.use(cors(CORS))
 
 app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
