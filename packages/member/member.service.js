7856ce764933bb0d91de25bfc230085ecb610996
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2020-09-04 22:39:27 +0700

    fix data

--- packages/member/member.service.js
+++ packages/member/member.service.js
@@ -2,6 +2,7 @@ import { USER_ERROR, MANGA } from "../../globalConstant/index";
 import * as logger from "../../util/logger";
 
 import { getMetaDataManga } from "../helper/manga.response";
+import * as ChapterRepository from "../repository/chapter.repository";
 import * as MangaRepository from "../repository/manga.repository";
 import * as MemberRepository from "../repository/member.repository";
 
@@ -30,37 +31,80 @@ export async function findByIdUserAndPopulate(id, query) {
         const skip = parseInt(query.skip) || 0
         let data = await MemberRepository.findByUserID(id);
         if (mangaFavorite == 1 && data.mangaFavorite.length > 0) {
-            let result = await getMetaDataManga(data.mangaFavorite, limit, skip);
-            return result;
+            let result = await MangaRepository.findArrayMangaMin(data.mangaFavorite, limit, skip > 0 ? (skip - 1) * limit : skip, { _id: -1 });
+            let tempManga = result;
+            let manga = await getMetaDataManga2(tempManga);
+            return { manga, total: data.mangaFavorite.length }
         }
         if (mangaSaved == 1 && data.mangaSaved.length > 0) {
-            let result = await getMetaDataManga(data.mangaSaved, limit, skip);
-            return result;
+            let result = await MangaRepository.findArrayMangaMin(data.mangaSaved, limit, skip > 0 ? (skip - 1) * limit : skip, { _id: -1 });
+            let tempManga = result;
+            let manga = await getMetaDataManga2(tempManga);
+            return { manga, total: data.mangaSaved.length }
         }
         if (chapterUpload == 1 && data.chapterUpload.length > 0) {
-            let result = await getMetaDataManga(data.chapterUpload, limit, skip);
-            return result;
+            let result = await MangaRepository.findArrayMangaMin(data.chapterUpload, limit, skip > 0 ? (skip - 1) * limit : skip, { _id: -1 });
+            let tempManga = result;
+            let manga = await getMetaDataManga2(tempManga);
+            return { manga, total: data.chapterUpload.length }
         }
         if (historyReading == 1 && data.historyReading.length > 0) {
-            let result = await getMetaDataManga(data.historyReading, limit, skip);
-            return result;
+            let result = await MangaRepository.findArrayMangaMin(data.historyReading, limit, skip > 0 ? (skip - 1) * limit : skip, { _id: -1 });
+            let tempManga = result;
+            let manga = await getMetaDataManga2(tempManga);
+            return { manga, total: data.historyReading.length }
         }
         if (mangaUpload == 1 && data.mangaUpload.length > 0) {
-            let result = await getMetaDataManga(data.mangaUpload, limit, skip);
-            return result;
+            let result = await MangaRepository.findArrayMangaMin(data.mangaUpload, limit, skip > 0 ? (skip - 1) * limit : skip, { _id: -1 });
+            let tempManga = result;
+            let manga = await getMetaDataManga2(tempManga);
+            return { manga, total: data.mangaUpload.length }
         }
         if (historyReadingChapter == 1 && data.historyReadingChapter.length > 0) {
             // let result = await getMetaDataManga(data.historyReadingChapter, limit, skip);
             return null;
         }
         if (mangaSuggested == 1 && data.mangaSuggested.length > 0) {
-            let result = await getMetaDataManga(data.mangaSuggested, limit, skip);
-            return result;
+            let result = await MangaRepository.findArrayMangaMin(data.mangaSuggested, limit, skip > 0 ? (skip - 1) * limit : skip, { _id: -1 });
+            let tempManga = result;
+            let manga = await getMetaDataManga2(tempManga);
+            return { manga, total: data.mangaSuggested.length }
         }
         return { manga: [], total: 0 };
     } catch (error) {
         logger.error(error);
         return Promise.reject(error);
+    }
+}
+
+
+async function getMetaDataManga2(manga) {
+    try {
+        let isArray = Array.isArray(manga);
+        if (!isArray) {
+            manga = [manga];
+        }
+        let promise = manga.map(async e => {
+            if (e.chapter) {
+                if (e.chapter.length > 4) {
+                    let lengthChapter = e.chapter.length;
+                    let ArrayChapterId = [];
+                    ArrayChapterId.push(e.chapter[0]);
+                    ArrayChapterId.push(e.chapter[lengthChapter - 3]);
+                    ArrayChapterId.push(e.chapter[lengthChapter - 2]);
+                    ArrayChapterId.push(e.chapter[lengthChapter - 1]);
+                    e.chapter = await ChapterRepository.findArrayChapter(ArrayChapterId);
+                } else {
+                    e.chapter = await ChapterRepository.findArrayChapter(e.chapter);
+                }
+            }
+            return e;
+        })
+        let data = await Promise.all(promise);
+        return isArray ? data : data[0];
+    } catch (error) {
+        logger.error(error);
+        return manga;
     }
 }
 
